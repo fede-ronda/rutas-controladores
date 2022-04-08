27b596ec1ac042e2318079ef8c890548c2317b29
Federico Rondanini <fede.ronda@gmail.com>
2022-04-08 14:37:20 -0300

    code

---
+++ routes/azul.js
@@ -0,0 +1,10 @@
+var express = require('express');
+const { append } = require('express/lib/response');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('azul', { title: 'Express' });
+});
+
+module.exports = router;
